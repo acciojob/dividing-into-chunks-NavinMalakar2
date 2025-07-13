dfe73f79e2fac1e6033fd54a83b724b005bab181
Navin Malakar <161839292+NavinMalakar2@users.noreply.github.com>
2025-07-13 20:39:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
-const divide = (arr, n) => {
-  // Write your code here
-};
+function divide(arr, maxSum) {
+  const result = [];
+  let currentChunk = []
+  let currentSum = 0;
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+  for (let num of arr) {
+    if (currentSum + num <= maxSum) {
+      currentChunk.push(num);
+      currentSum += num;
+    } else {
+      result.push(currentChunk);
+      currentChunk = [num];
+      currentSum = num;
+    }
+  }
+
+  // Add last chunk if not empty
+  if (currentChunk.length > 0) {
+    result.push(currentChunk);
+  }
+
+  return result;
+}
+
+
+const maxSum = prompt("Enter n: ");
+alert(JSON.stringify(divide(arr, maxSum)));
